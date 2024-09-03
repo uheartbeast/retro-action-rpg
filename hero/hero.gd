070ea94720ff576971e3a367b654d6f6c1320d69
Heartbeast <heartbeast.studios@gmail.com>
2024-09-03 10:33:59 -0600

    4 - Animating the Hero

--- hero/hero.gd
+++ hero/hero.gd
@@ -11,7 +11,8 @@ var facing_direction: = Vector2.DOWN :
 		else:
 			value = Vector2(0, sign(value.y))
 		facing_direction = value
-		print(facing_direction)
+		
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	motion_mode = MOTION_MODE_FLOATING
@@ -19,5 +20,24 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	facing_direction = input_vector
+	if input_vector != Vector2.ZERO:
+		play_animation("run")
+	else:
+		play_animation("idle")
 	velocity = input_vector * 100
 	move_and_slide()
+
+func play_animation(animation: String) -> void:
+	var animation_name: = animation + "_" + get_direction_string()
+	animation_player.play(animation_name)
+
+func get_direction_string() -> String:
+	var direction_string: = ""
+	if facing_direction.x == 0.0:
+		if facing_direction.y < 0.0:
+			direction_string = "up"
+		else:
+			direction_string = "down"
+	else:
+		direction_string = "side"
+	return direction_string
