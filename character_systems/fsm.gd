fd34f71f097de1cbbc2cdc10783364e53149f86a
Heartbeast <heartbeast.studios@gmail.com>
2024-09-04 10:46:25 -0600

    9 - Finite State Machine

---
+++ character_systems/fsm.gd
@@ -0,0 +1,14 @@
+class_name FSM
+extends RefCounted
+
+var state: State : set = set_state
+
+func set_state(value: State) -> FSM:
+	state = value
+	state.enter()
+	return self
+
+func change_state(new_state: State) -> void:
+	state.exit()
+	state = new_state
+	state.enter()
