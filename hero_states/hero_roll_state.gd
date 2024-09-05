a8198ae61c1e21ed6287f1e114dc2bda2c16fec0
Heartbeast <heartbeast.studios@gmail.com>
2024-09-05 10:54:03 -0600

    13 - Hero Roll State

---
+++ hero_states/hero_roll_state.gd
@@ -0,0 +1,14 @@
+class_name HeroRollState
+extends State
+
+func enter() -> void:
+	var hero: = actor as Hero
+	hero.play_animation("roll")
+	hero.animation_player.animation_finished.connect(func(_animation_name: String):
+		finished.emit()
+	, CONNECT_ONE_SHOT)
+
+func physics_process(delta: float) -> void:
+	var hero: = actor as Hero
+	CharacterMover.accelerate_in_direction(hero, hero.facing_direction, hero.movement_stats, delta)
+	CharacterMover.move(hero)
