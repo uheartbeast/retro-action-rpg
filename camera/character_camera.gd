68690a53b210b89145f0d961262ca5fd3b6d96fd
Heartbeast <heartbeast.studios@gmail.com>
2024-09-06 10:21:18 -0600

    19 - Character Camera

---
+++ camera/character_camera.gd
@@ -0,0 +1,11 @@
+class_name CharacterCamera
+extends Camera2D
+
+# We don't want anything directly accessing the camera target
+var _target: RemoteTransform2D : set = set_target
+
+func set_target(value: RemoteTransform2D) -> void:
+	if _target is RemoteTransform2D:
+		_target.remote_path = ""
+	_target = value
+	_target.remote_path = get_path()
