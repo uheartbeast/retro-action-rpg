d412636685649cf3391f2a26806082ddfd9cfb5d
Heartbeast <heartbeast.studios@gmail.com>
2024-09-06 14:13:41 -0600

    Level

---
+++ levels/level.gd
@@ -0,0 +1,5 @@
+class_name Level
+extends Node2D
+
+func _ready() -> void:
+	y_sort_enabled = true
---
+++ world.gd
@@ -0,0 +1,5 @@
+class_name World
+extends Node2D
+
+func _ready() -> void:
+	y_sort_enabled = true
