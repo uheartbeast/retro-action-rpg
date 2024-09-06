c6276a10bce01954bc3073f39bf776063ea86067
Heartbeast <heartbeast.studios@gmail.com>
2024-09-06 10:00:22 -0600

    17 - Hero Weapon State

--- hero_states/hero_weapon_state.gd
+++ hero_states/hero_weapon_state.gd
@@ -1,13 +1,13 @@
-class_name HeroRollState
+class_name HeroWeaponState
 extends State
 
 func enter() -> void:
 	var hero: = actor as Hero
-	hero.play_animation("roll")
+	hero.play_animation("sword")
 	await hero.animation_player.animation_finished
 	finished.emit()
 
 func physics_process(delta: float) -> void:
 	var hero: = actor as Hero
-	CharacterMover.accelerate_in_direction(hero, hero.direction, hero.roll_movement_stats, delta)
+	CharacterMover.decelerate(hero, hero.movement_stats, delta)
 	CharacterMover.move(hero)
