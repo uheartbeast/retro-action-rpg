97acc38660f6ae494054dcfae182a0712bbbb4ad
Heartbeast <heartbeast.studios@gmail.com>
2024-09-09 16:26:12 -0600

    33 - Navigation Agent in the Enemy Chase State

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -1,7 +1,12 @@
 class_name BatEnemy
 extends Enemy
 
-@onready var chase_state: = EnemyChaseState.new().set_actor(self)
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var chase_state: EnemyChaseState = (
+	EnemyChaseState.new()
+	.set_actor(self)
+	.set_navigation_agent(navigation_agent_2d)
+)
 @onready var fsm: = FSM.new().set_state(chase_state)
 
 func _physics_process(delta: float) -> void:
--- enemy_states/enemy_chase_state.gd
+++ enemy_states/enemy_chase_state.gd
@@ -1,10 +1,19 @@
 class_name EnemyChaseState
 extends State
 
+var navigation_agent: NavigationAgent2D : set = set_navigation_agent
+
+func set_navigation_agent(value: NavigationAgent2D) -> EnemyChaseState:
+	navigation_agent = value
+	return self
+
 func physics_process(delta: float) -> void:
 	var enemy: = actor as Enemy
 	var hero: = MainInstances.hero as Hero
 	if hero is not Hero: return
-	var direction: = enemy.global_position.direction_to(hero.global_position)
+	
+	navigation_agent.target_position = hero.global_position
+	var next_point: = navigation_agent.get_next_path_position()
+	var direction: = enemy.global_position.direction_to(next_point)
 	CharacterMover.accelerate_in_direction(enemy, direction, enemy.movement_stats, delta)
 	CharacterMover.move(enemy)
