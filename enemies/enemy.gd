7caa213b66a5ff86677485a6e42d6360482cef9b
Heartbeast <heartbeast.studios@gmail.com>
2024-09-09 10:59:24 -0600

    26 - Enemy

---
+++ enemies/enemy.gd
@@ -0,0 +1,6 @@
+class_name Enemy
+extends CharacterBody2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
