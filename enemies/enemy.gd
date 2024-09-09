aba672c547351abf98b3b9dd55305930f67cf22d
Heartbeast <heartbeast.studios@gmail.com>
2024-09-09 11:41:07 -0600

    31 - Bat Movement Stats and Collision Layers

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,6 +1,12 @@
 class_name Enemy
 extends CharacterBody2D
 
+@export var movement_stats: MovementStats
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	motion_mode = MOTION_MODE_FLOATING
+	assert(movement_stats is MovementStats, "ERROR: No movement stats set on enemy: " + str(name))
--- enemy_states/enemy_chase_state.gd
+++ enemy_states/enemy_chase_state.gd
@@ -5,7 +5,6 @@ func physics_process(delta: float) -> void:
 	var enemy: = actor as Enemy
 	var hero: = MainInstances.hero as Hero
 	if hero is not Hero: return
-	var movement_stats: = MovementStats.new()
 	var direction: = enemy.global_position.direction_to(hero.global_position)
-	CharacterMover.accelerate_in_direction(enemy, direction, movement_stats, delta)
+	CharacterMover.accelerate_in_direction(enemy, direction, enemy.movement_stats, delta)
 	CharacterMover.move(enemy)
