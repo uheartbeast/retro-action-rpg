37704e84d68439a3d8b2e23f8d6d047ff0f13fb1
Heartbeast <heartbeast.studios@gmail.com>
2024-09-13 09:33:05 -0600

    55 - Hurtbox Monitoring

--- damage_boxes/hurtbox.gd
+++ damage_boxes/hurtbox.gd
@@ -1,6 +1,9 @@
 class_name Hurtbox
 extends Area2D
 
-var is_invincible: = false
+var is_invincible: = false :
+	set(value):
+		is_invincible = value
+		set_deferred("monitoring", not is_invincible)
 
 signal hurt(hitbox: Hitbox)
