064f76965e526518daa740f9bc0f745f71c012f5
Heartbeast <heartbeast.studios@gmail.com>
2024-09-16 11:27:37 -0600

    61 - Finalizing the Hero Roll State

--- hero_states/hero_roll_state.gd
+++ hero_states/hero_roll_state.gd
@@ -3,6 +3,7 @@ extends State
 
 func enter() -> void:
 	var hero: = actor as Hero
+	hero.hurtbox.is_invincible = true
 	hero.play_animation("roll")
 	await hero.animation_player.animation_finished
 	finished.emit()
@@ -11,3 +12,7 @@ func physics_process(delta: float) -> void:
 	var hero: = actor as Hero
 	CharacterMover.accelerate_in_direction(hero, hero.direction, hero.roll_movement_stats, delta)
 	CharacterMover.move(hero)
+
+func exit() -> void:
+	var hero: = actor as Hero
+	hero.hurtbox.is_invincible = false
