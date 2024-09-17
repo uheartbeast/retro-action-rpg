95885973afec6fa06d45d1bead0960b8172150df
Heartbeast <heartbeast.studios@gmail.com>
2024-09-17 10:26:51 -0600

    68 - Item

---
+++ items/item.gd
@@ -0,0 +1,8 @@
+class_name Item
+extends Resource
+
+@export var name: String
+@export var plural_name: String
+@export var icon: Texture
+@export_multiline var description: String
+@export var is_equipment: = false
