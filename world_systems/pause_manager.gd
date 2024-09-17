4eb90266ffb9322fffa505ee059a192be5596511
Heartbeast <heartbeast.studios@gmail.com>
2024-09-17 10:20:04 -0600

    67 - Pause Manager

---
+++ world_systems/pause_manager.gd
@@ -0,0 +1,17 @@
+class_name PauseManager
+extends Node
+
+signal paused()
+signal unpaused()
+
+var is_paused: = false :
+	set(value):
+		is_paused = value
+		get_tree().paused = is_paused
+
+func _ready() -> void:
+	process_mode = PROCESS_MODE_ALWAYS
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		is_paused = not is_paused
