ab66465e74f010a8f47dfb9a6dd145bcca7f0059
Heartbeast <heartbeast.studios@gmail.com>
2024-09-18 10:58:00 -0600

    80 - Inventory UI

---
+++ ui/inventory_ui.gd
@@ -0,0 +1,34 @@
+class_name InventoryUI
+extends GridContainer
+
+const INVENTORY_SLOT_UI = preload("res://ui/inventory_slot_ui.tscn")
+
+var inventory_slot_index: = 0
+
+signal inventory_slot_selected(inventory_ui, slot, event)
+
+var inventory: Inventory = null :
+	set(value):
+		inventory = value
+		if inventory is not Inventory: return
+		update_inventory_grid.call_deferred()
+
+func update_inventory_grid() -> void:
+	clear_inventory_slots()
+	fill_inventory_slots()
+
+func clear_inventory_slots() -> void:
+	for child in get_children():
+		child.queue_free()
+
+func fill_inventory_slots() -> void:
+	for item_box: ItemBox in inventory.get_item_boxes():
+		var inventory_slot_ui: = INVENTORY_SLOT_UI.instantiate()
+		add_child(inventory_slot_ui)
+		inventory_slot_ui.selected.connect(func(inventory_slot_ui: InventorySlotUI, event: InputEvent):
+			inventory_slot_selected.emit(self, inventory_slot_ui, event)
+		)
+		inventory_slot_ui.focus_entered.connect(func():
+			inventory_slot_index = inventory_slot_ui.get_index()
+		)
+		inventory_slot_ui.item_box = item_box
