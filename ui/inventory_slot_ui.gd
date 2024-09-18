f58f6cabe3be0931d24c5c243b47eb100c21fcbf
Heartbeast <heartbeast.studios@gmail.com>
2024-09-18 10:27:38 -0600

    78 - Inventory Slot UI Scene

---
+++ ui/inventory_slot_ui.gd
@@ -0,0 +1,8 @@
+class_name InventorySlotUI
+extends Button
+
+@onready var label: Label = $Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
