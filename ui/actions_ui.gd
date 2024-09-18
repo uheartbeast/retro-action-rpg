b4a36762632ae9c91c802c47fcda15b06d8554b7
Heartbeast <heartbeast.studios@gmail.com>
2024-09-18 13:56:34 -0600

    87 - Actions UI Scene

---
+++ ui/actions_ui.gd
@@ -0,0 +1,9 @@
+class_name ActionsUI
+extends VBoxContainer
+
+@onready var action_slot_ui_1: ActionSlotUI = %ActionSlotUI1
+@onready var action_slot_ui_2: ActionSlotUI = %ActionSlotUI2
+@onready var action_slot_ui_3: ActionSlotUI = %ActionSlotUI3
+
+func _ready() -> void:
+	pass # Replace with function body.
