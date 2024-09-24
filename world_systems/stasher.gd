db50552055ece3758278b3d3aca69b4d59398223
Heartbeast <heartbeast.studios@gmail.com>
2024-09-24 10:00:20 -0600

    112 - Stasher

---
+++ world_systems/stasher.gd
@@ -0,0 +1,21 @@
+class_name Stasher
+extends RefCounted
+
+var target: set = set_target
+var starting_position: Vector2
+
+func set_target(value: Node2D) -> Stasher:
+	target = value
+	if target is not Node2D: return self
+	starting_position = target.global_position
+	return self
+
+func get_id() -> String:
+	var world: = target.get_tree().current_scene as World
+	assert(world is World, "You can't get the id for an object ouside of the world scene.")
+	var id: String = (
+		target.scene_file_path
+		+ " at " + str(starting_position)
+		+ " in " + world.current_level.scene_file_path
+	)
+	return id
