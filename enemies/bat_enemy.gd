25183b7fe5f24f84ecb26bda66affdd9dd247f1a
Heartbeast <heartbeast.studios@gmail.com>
2024-09-24 10:18:15 -0600

    114 - Persistent Bats

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -10,14 +10,20 @@ extends Enemy
 @onready var knockback_state: = EnemyKnockbackState.new().set_actor(self)
 @onready var fsm: = FSM.new().set_state(chase_state)
 
+@onready var stasher: = Stasher.new().set_target(self)
+
 func _ready() -> void:
 	super()
+	if stasher.retrieve_property("freed"): queue_free()
 	hurtbox.hurt.connect(func(other_hitbox: Hitbox):
 		fsm.change_state(knockback_state.set_knockback(other_hitbox.knockback))
 		create_hit_particles(other_hitbox, load("res://effects/hit_particles.tscn"))
 		stats.health -= other_hitbox.damage
 	)
 	knockback_state.finished.connect(fsm.change_state.bind(chase_state))
+	tree_exiting.connect(func():
+		stasher.stash_property("freed", true)
+	)
 
 func _physics_process(delta: float) -> void:
 	fsm.state.physics_process(delta)
