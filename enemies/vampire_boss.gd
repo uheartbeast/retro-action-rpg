9f905f2893ce53ea3c9bd0336fb029d8e9895522
Heartbeast <heartbeast.studios@gmail.com>
2024-09-26 11:24:04 -0600

    137 - Implementing the Move to Random Marker State

--- enemies/vampire_boss.gd
+++ enemies/vampire_boss.gd
@@ -1,11 +1,31 @@
 class_name VampireBoss
 extends Enemy
 
+const EXPLOSION_EFFECT_SCENE: = preload("res://effects/explosion_effect.tscn")
 
-@onready var fsm: FSM = FSM.new()
+@export var target_markers: Array[Marker2D]
+
+@onready var pause_state: = EnemyPauseState.new().set_actor(self)
+@onready var move_to_random_marker_state: = (
+	EnemyMoveToRandomMarkerState.new()
+	.set_markers(target_markers)
+	.set_actor(self)
+)
+@onready var fsm: FSM = FSM.new().set_state(pause_state)
 
 func _ready() -> void:
-	pass
+	pause_state.finished.connect(fsm.change_state.bind(move_to_random_marker_state))
+	move_to_random_marker_state.finished.connect(fsm.change_state.bind(pause_state))
+	
+	hurtbox.hurt.connect(func(other_hitbox: Hitbox):
+		var damage = other_hitbox.damage
+		await flasher.flash()
+		await get_tree().create_timer(0.1).timeout
+		stats.health -= damage
+		if stats.is_health_gone():
+			Utils.instantiate_scene_on_level(EXPLOSION_EFFECT_SCENE, global_position)
+			queue_free()
+	)
 
 func _physics_process(delta: float) -> void:
 	fsm.state.physics_process(delta)
--- enemy_states/enemy_move_to_random_marker_state.gd
+++ enemy_states/enemy_move_to_random_marker_state.gd
@@ -1,4 +1,4 @@
-class_name EnemyMoveToRandomMarker
+class_name EnemyMoveToRandomMarkerState
 extends State
 
 const REACHED_TARGET_MARGIN: = 8
@@ -7,7 +7,7 @@ var markers: Array[Marker2D] : set = set_markers
 var target_positions: Array[Vector2]
 var target_position: Vector2
 
-func set_markers(value: Array[Marker2D]) -> EnemyMoveToRandomMarker:
+func set_markers(value: Array[Marker2D]) -> EnemyMoveToRandomMarkerState:
 	markers = value
 	return self
 
@@ -25,6 +25,7 @@ func enter() -> void:
 func physics_process(delta: float) -> void:
 	var enemy: = actor as Enemy
 	var direction: Vector2 = enemy.global_position.direction_to(target_position)
+	enemy.animation_player.play("move")
 	if direction.x != 0:
 		enemy.sprite_2d.scale.x = sign(direction.x)
 	
